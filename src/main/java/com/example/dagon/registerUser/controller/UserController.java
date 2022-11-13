db5e1c95e99576e5eda627748065aa2d08a1947b
dagon derik farias dantas <dagonderik@gmail.com>
2022-11-12 21:33:27 -0300

    Add PUT and DELETE methods

--- src/main/java/com/example/dagon/registerUser/controller/UserController.java
+++ src/main/java/com/example/dagon/registerUser/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.dagon.registerUser.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.dagon.registerUser.model.User;
 import com.example.dagon.registerUser.services.UserService;
 
+@CrossOrigin
 @RestController
 public class UserController {
 
@@ -28,8 +30,18 @@ public class UserController {
         return userService.getUser(id);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value ="/users/{id}")
+    public void putUser(@RequestBody User user) {
+        userService.editUser(user, user.getId());
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/users")
     public void addUser(@RequestBody User user) {
         userService.addUser(user);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/users/{id}")
+    public void deleteUser(@PathVariable ("id") String id) {
+        userService.deleteUser(id);
     }
 }
--- src/main/java/com/example/dagon/registerUser/services/UserService.java
+++ src/main/java/com/example/dagon/registerUser/services/UserService.java
@@ -11,23 +11,38 @@ import com.example.dagon.registerUser.model.User;
 @Service
 public class UserService {
 
-    private List<User> userList = new ArrayList<>(Arrays.asList(
-
-        new User("1", "_Spring FrameWork", "_Spring Description"),
-        new User("12", "Spring FrameWork", "Spring Description"),
-        new User("3", "Java FrameWork", "Java Description")
-    ));
+    private List<User> userList = new ArrayList<>(Arrays.asList());
+    private int id = 1;
 
     public List<User> getAllUserList() {
         return userList;
     }
 
     public User getUser(String id) {
-        return userList.stream().filter( user -> user.getId().equals(id)).findFirst().get();
+        return userList.stream().filter(user -> user.getId().equals(id)).findFirst().get();
     }
 
     public void addUser(User user) {
+        user.setId(Integer.toString(id));
+        id++;
         userList.add(user);
     }
-    
+
+    public void editUser(User user, String id) {
+        userList.stream().filter(a -> a.getId() == user.getId()).
+        findAny().ifPresent(a -> {
+            a.setName("zzzzzzzzzz");
+            a.setEmail("zzzzzzzzzzzzz");
+        });
+        // userList.stream().filter(a -> a.getId() == id).
+        // findAny().ifPresent(a -> {
+        //     a.setName(user.getName()); 
+        //     a.setEmail(user.getEmail());
+        // });
+    }
+
+    public void deleteUser(String id) {
+        userList.removeIf(user -> user.getId().equals(id));
+    }
+
 }
